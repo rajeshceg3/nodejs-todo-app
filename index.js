8a1ff7647c62f629b1d43af42efe2d24fb3fffaa
rajeshceg3 <rajeshceg3@gmail.com>
2022-12-08 19:35:52 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
-const path = require('path');
+const mongodb = require('mongodb');
+const mongodbMemoryServer = require('mongodb-memory-server');
 
-app.use(bodyParser.json());
-const todos = []
+ function wrapper( async () =>{
+  const mongoServer = new mongodbMemoryServer.MongoMemoryServer();
+  await mongoServer.ensureInstance();                      
+  const mongoUri = mongoServer.getUri();
+})
 
-app.post('/list', (req,res)=>{
-  const content = req.body.text;
-  todos.push({content,});
 
-  res.status(201).send({
-    todo : todos[todos.length - 1]
+mongodb.MongoClient.connect( mongoUri, { useNewUrlParser:true})
+  .then( client => {
+    global.client = client;    
+    const app = express();
+    const port = 3000;
+    const path = require('path');
+    
+    app.use(bodyParser.json());
+//    const todos = []
+    
+    app.post('/list', (req,res)=>{
+      const content = req.body.text;
+      client.db('prod').collection('list').insertOne({content,});
+      res.status(201).send({
+        todo : {content},
+      });
+    });
+    
+    
+    app.get('/list', (req, res) => {
+      client.db('prod').collection('list').find({}).toArray()
+        .then( todos =>{
+          res.status(200).send({
+            todos,
+          })
+        })
+    });
+    
+    app.listen(port, () => {
+      console.log(` App listening at http://localhost:${port}`);
+    });
+    
+
   });
-});
-
-
-app.get('/list', (req, res) => {
- res.status(200).send({
-   todos,
- })
-});
-
-app.listen(port, () => {
-  console.log(` App listening at http://localhost:${port}`);
-});
