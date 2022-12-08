11756ddd3c3a2c5d10d65557229b6e9a0e81ca86
rajeshceg3 <rajeshceg3@gmail.com>
2022-12-08 17:00:37 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3010;
 const path = require('path');
 
-app.use(express.static('static'));
+app.use(bodyParser.json());
+const todos = []
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('pages/index.html'));
+app.post('/list', (req,res)=>{
+  const content = req.body.text;
+  todos.push({content,});
+
+  res.status(201).send({
+    todo : todos[todos.length - 1]
+  });
+});
+
+
+app.get('/list', (req, res) => {
+ res.status(200).send({
+   todos,
+ })
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(` App listening at http://localhost:${port}`);
 });
