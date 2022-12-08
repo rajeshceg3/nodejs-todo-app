37da95680073fba8cf6af4471e2ac51fa07aed66
rajeshceg3 <rajeshceg3@gmail.com>
2022-12-08 17:10:10 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3010;
+const port = 3000;
 const path = require('path');
 
 app.use(bodyParser.json());
